3e7c24f2585e0e0b8caf0b84670b3c8b2d49f66f
Gabe Rojas-Westall <gaber@remitly.com>
2022-12-01 10:09:48 -0600

    aoc 22

---
+++ 2022/day_1/main.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"sort"
+	"strconv"
+)
+
+func main() {
+	sortedCals := getSortedCals()
+	partOne(sortedCals)
+	partTwo(sortedCals)
+}
+
+func partOne(sortedCals []int) {
+	fmt.Printf("part one: %v\n", sortedCals[len(sortedCals)-1])
+}
+
+func partTwo(sortedCals []int) {
+	sumOfHighestThreeElves := 0
+	for i := 1; i < 4; i++ {
+		sumOfHighestThreeElves += sortedCals[len(sortedCals)-i]
+	}
+	fmt.Printf("part two: %v\n", sumOfHighestThreeElves)
+}
+
+func getSortedCals() []int {
+	// open file
+	// f, err := os.Open("~/Documents/GitHub/rojaswestall/advent-of-code/2022/day_1/input.txt")
+	f, err := os.Open("input.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer f.Close()
+
+	// read the file line by line using scanner
+	scanner := bufio.NewScanner(f)
+
+	var highestCals []int
+	currentElfCalories := 0
+	for scanner.Scan() {
+		cal, err := strconv.Atoi(scanner.Text())
+		if err != nil {
+			// empty line
+			highestCals = append(highestCals, currentElfCalories)
+			currentElfCalories = 0
+			continue
+		}
+		currentElfCalories += cal
+	}
+
+	sort.Ints(highestCals)
+	return highestCals
+}
