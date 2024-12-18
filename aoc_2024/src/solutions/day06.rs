549e263ea4fc2496c9185b488580c28145be2ba2
Gabriel Rojas-Westall <rojaswestall@gmail.com>
2024-12-18 15:33:56 -0600

    day 10

--- aoc_2024/src/solutions/day06.rs
+++ aoc_2024/src/solutions/day06.rs
@@ -1,6 +1,7 @@
-use std::{collections::HashSet, fmt};
+use std::collections::HashSet;
 
 use crate::utils::file;
+use crate::utils::direction;
 
 const DAY_6_INPUT: &str = "src/input/day06.txt";
 const OPEN: &str = ".";
@@ -19,7 +20,7 @@ fn part1() -> i32 {
 
     // I cheated and looked at the input. You could write some helper
     // methods to determine the direction and not just the location
-    let mut cur_dir = Direction::Up;
+    let mut cur_dir = direction::Direction::Up;
     let (mut x, mut y) = find_start(&matrix);
 
     let mut visited = Vec::new();
@@ -31,7 +32,8 @@ fn part1() -> i32 {
         }
 
         // get next position, if next position is out of bounds exit
-        let (next_x, next_y) = match get_next_pos(&cur_dir, &x, &y, &max_x, &max_y) {
+        let (next_x, next_y) = match cur_dir.get_next_pos(&x, &y, &max_x, &max_y) {
+            // let (next_x, next_y) = match get_next_pos(&cur_dir, &x, &y, &max_x, &max_y) {
             Ok((x, y)) => (x, y),
             Err(_e) => {
                 // next position is out of bounds
@@ -60,7 +62,7 @@ fn part2() -> i32 {
 
     // I cheated and looked at the input. You could write some helper
     // methods to determine the direction and not just the location
-    let mut cur_dir = Direction::Up;
+    let mut cur_dir = direction::Direction::Up;
     let (mut x, mut y) = find_start(&matrix);
 
     // we want to know which positions will create loops, so we also
@@ -73,7 +75,7 @@ fn part2() -> i32 {
         visited.insert((x, y));
 
         // get next position, if next position is out of bounds exit
-        let (next_x, next_y) = match get_next_pos(&cur_dir, &x, &y, &max_x, &max_y) {
+        let (next_x, next_y) = match cur_dir.get_next_pos(&x, &y, &max_x, &max_y) {
             Ok((x, y)) => (x, y),
             Err(_e) => {
                 // next position is out of bounds
@@ -107,7 +109,7 @@ fn part2() -> i32 {
     return loop_obstacles.len().try_into().unwrap();
 }
 
-fn is_loop(d: &Direction, x: usize, y: usize, matrix: &Vec<Vec<String>>) -> bool {
+fn is_loop(d: &direction::Direction, x: usize, y: usize, matrix: &Vec<Vec<String>>) -> bool {
     let max_x = matrix[0].len() - 1;
     let max_y = matrix.len() - 1;
     let mut cur_dir = *d;
@@ -117,11 +119,11 @@ fn is_loop(d: &Direction, x: usize, y: usize, matrix: &Vec<Vec<String>>) -> bool
     // we want to know which positions will create loops, so we also
     // need to keep track of the direction we were going when we visited
     // a specific position. It's a loop if we see the same visited obstacle
-    let mut visited_obstacles: Vec<(usize, usize, Direction)> = Vec::new();
+    let mut visited_obstacles: Vec<(usize, usize, direction::Direction)> = Vec::new();
 
     loop {
         // get next position, if next position is out of bounds exit
-        let (next_x, next_y) = match get_next_pos(&cur_dir, &cur_x, &cur_y, &max_x, &max_y) {
+        let (next_x, next_y) = match cur_dir.get_next_pos(&cur_x, &cur_y, &max_x, &max_y) {
             Ok((x, y)) => (x, y),
             Err(_e) => {
                 // next position is out of bounds
@@ -158,67 +160,11 @@ fn find_start(matrix: &Vec<Vec<String>>) -> (usize, usize) {
     return (0, 0);
 }
 
-fn get_next_pos(
-    d: &Direction,
-    x: &usize,
-    y: &usize,
-    max_x: &usize,
-    max_y: &usize,
-) -> Result<(usize, usize), String> {
-    let error_str = String::from("out of bounds");
+fn get_next_direction(d: &direction::Direction) -> direction::Direction {
     match d {
-        Direction::Up => {
-            if *y == 0 {
-                return Err(error_str);
-            }
-            Ok((*x, y - 1))
-        }
-        Direction::Right => {
-            if x == max_x {
-                return Err(error_str);
-            }
-            Ok((x + 1, *y))
-        }
-        Direction::Down => {
-            if y == max_y {
-                return Err(error_str);
-            }
-            Ok((*x, y + 1))
-        }
-        Direction::Left => {
-            if *x == 0 {
-                return Err(error_str);
-            }
-            Ok((x - 1, *y))
-        }
-    }
-}
-
-#[derive(PartialEq, Copy, Clone)]
-enum Direction {
-    Up,
-    Right,
-    Down,
-    Left,
-}
-
-// Implement the Display trait for the Direction enum
-impl fmt::Display for Direction {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        match self {
-            Direction::Up => write!(f, "Up"),
-            Direction::Right => write!(f, "Right"),
-            Direction::Down => write!(f, "Down"),
-            Direction::Left => write!(f, "Left"),
-        }
-    }
-}
-
-fn get_next_direction(d: &Direction) -> Direction {
-    match d {
-        Direction::Up => Direction::Right,
-        Direction::Right => Direction::Down,
-        Direction::Down => Direction::Left,
-        Direction::Left => return Direction::Up,
+        direction::Direction::Up => direction::Direction::Right,
+        direction::Direction::Right => direction::Direction::Down,
+        direction::Direction::Down => direction::Direction::Left,
+        direction::Direction::Left => return direction::Direction::Up,
     }
 }
---
+++ aoc_2024/src/utils/direction.rs
@@ -0,0 +1,59 @@
+use std::fmt;
+
+#[derive(PartialEq, Copy, Clone)]
+pub enum Direction {
+    Up,
+    Right,
+    Down,
+    Left,
+}
+
+impl Direction {
+    pub fn get_next_pos(
+        &self,
+        x: &usize,
+        y: &usize,
+        max_x: &usize,
+        max_y: &usize,
+    ) -> Result<(usize, usize), String> {
+        let error_str = String::from("out of bounds");
+        match self {
+            Direction::Up => {
+                if *y == 0 {
+                    return Err(error_str);
+                }
+                Ok((*x, y - 1))
+            }
+            Direction::Right => {
+                if x == max_x {
+                    return Err(error_str);
+                }
+                Ok((x + 1, *y))
+            }
+            Direction::Down => {
+                if y == max_y {
+                    return Err(error_str);
+                }
+                Ok((*x, y + 1))
+            }
+            Direction::Left => {
+                if *x == 0 {
+                    return Err(error_str);
+                }
+                Ok((x - 1, *y))
+            }
+        }
+    }
+}
+
+// Implement the Display trait for the Direction enum
+impl fmt::Display for Direction {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            Direction::Up => write!(f, "Up"),
+            Direction::Right => write!(f, "Right"),
+            Direction::Down => write!(f, "Down"),
+            Direction::Left => write!(f, "Left"),
+        }
+    }
+}
